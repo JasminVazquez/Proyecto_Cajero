7782385b28f6358fcb29fddb82f66db51972c371
CesarRick3 <l20170762@culiacan.tecnm.mx>
2023-12-04 19:48:49 -0700

    Agregamos nuevos metodos necesarios para el uso

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     static Scanner entrada = new Scanner(System.in);
@@ -19,7 +20,7 @@ public class Main {
             System.out.println("0. Salir");
             System.out.print("Ingrese la opción: ");
             opcion = entrada.nextInt();
-            entrada.nextLine(); // Limpiar el buffer de entrada
+            entrada.nextLine(); 
             
             switch (opcion) {
                 case 1:
@@ -43,7 +44,7 @@ public class Main {
         System.out.println("\nRegistro de nuevo cliente:");
         System.out.print("Ingrese el identificador del cliente: ");
         int identificador = entrada.nextInt();
-        entrada.nextLine(); // Limpiar el buffer de entrada
+        entrada.nextLine(); 
         
         System.out.print("Ingrese el nombre del cliente: ");
         String nombre = entrada.nextLine();
@@ -51,10 +52,8 @@ public class Main {
         System.out.print("Ingrese la dirección del cliente: ");
         String direccion = entrada.nextLine();
         
-        // Crear un nuevo objeto Cliente con los datos ingresados
         Cliente nuevoCliente = new Cliente(identificador, nombre, direccion);
         
-        // Agregar el nuevo cliente a la lista de clientes
         clientes.add(nuevoCliente);
         
         System.out.println("¡Registro exitoso!");
@@ -66,7 +65,7 @@ public class Main {
 
         System.out.print("Ingrese el identificador del cliente: ");
         int identificadorCliente = entrada.nextInt();
-        entrada.nextLine(); // Limpiar el buffer de entrada
+        entrada.nextLine(); 
         
         // Buscar el cliente en la lista utilizando su identificador
         Cliente clienteEncontrado = buscarClientePorIdentificador(identificadorCliente);
@@ -98,14 +97,37 @@ public static void mostrarOpcionesOperaciones(Cliente cliente) {
 
     System.out.print("Ingrese la opción deseada: ");
     int opcionOperacion = entrada.nextInt();
-    entrada.nextLine(); // Limpiar el buffer de entrada
+    entrada.nextLine(); 
 
     switch (opcionOperacion) {
         case 1:
-            // Implementar lógica para crear nueva cuenta de débito
+            System.out.println("\nCreación de nueva cuenta de débito:");
+            System.out.print("Ingrese el tipo de cuenta (Cuenta de Ahorro o Cuenta de Cheques): ");
+            String tipoCuenta = entrada.nextLine();
+        
+            // Crear una nueva cuenta de débito asociada al cliente actual
+            CuentaDebito nuevaCuenta = new CuentaDebito(tipoCuenta, cliente, generarNumeroCuenta());
+            cuentas.add(nuevaCuenta);
+        
+            System.out.println("¡Cuenta de débito creada exitosamente!");
             break;
+            
         case 2:
-            // Implementar lógica para usar cuenta de débito existente
+            System.out.println("\nUso de cuenta de débito existente:");
+            System.out.print("Ingrese el número de cuenta de débito: ");
+            int numeroCuentaExistente = entrada.nextInt();
+            entrada.nextLine(); 
+        
+            // Buscar la cuenta de débito en la lista utilizando su número
+            CuentaDebito cuentaExistente = buscarCuentaPorNumero(numeroCuentaExistente);
+        
+            if (cuentaExistente != null && cuentaExistente.getCliente().equals(cliente)) {
+                System.out.println("¡Usted está utilizando la cuenta: " + cuentaExistente.getNumeroCuenta() +
+                        " - " + cuentaExistente.getTipo() + "!");
+                // Implementar operaciones adicionales utilizando esta cuenta
+            } else {
+                System.out.println("La cuenta no existe o no está asociada a este cliente.");
+            }
             break;
         case 0:
             System.out.println("Sesión cerrada. ¡Hasta luego!");
@@ -116,29 +138,44 @@ public static void mostrarOpcionesOperaciones(Cliente cliente) {
             break;
     }
 }
+// Función para generar un número de cuenta único
+public static int generarNumeroCuenta() {
+        Random random = new Random();
+        int numeroAleatorio = random.nextInt(91) + 10; // Genera un número aleatorio entre 0 y 90 y luego suma 10
+        return numeroAleatorio;
+}
+
+// Función para buscar una cuenta de débito por su número
+public static CuentaDebito buscarCuentaPorNumero(int numeroCuenta) {
+    for (CuentaDebito cuenta : cuentas) {
+        if (cuenta.getNumeroCuenta() == numeroCuenta) {
+            return cuenta;
+        }
+    }
+    return null; // Retorna null si no se encuentra la cuenta
+}
 
     // Función para inicializar datos de ejemplo
     public static void inicializarDatos() {
         // Crear algunos clientes y cuentas de débito de ejemplo
         Cliente cliente1 = new Cliente(1, "Carlos", "Calle A");
         Cliente cliente2 = new Cliente(2, "María", "Calle B");
-        // Agregar más clientes si es necesario...
+        
 
         CuentaDebito cuenta1 = new CuentaDebito("Cuenta de Ahorro", cliente1, 101);
         CuentaDebito cuenta2 = new CuentaDebito("Cuenta de Cheques", cliente1, 102);
         CuentaDebito cuenta3 = new CuentaDebito("Cuenta de Ahorro", cliente2, 201);
         CuentaDebito cuenta4 = new CuentaDebito("Cuenta de Cheques", cliente2, 202);
-        // Agregar más cuentas si es necesario...
+        
 
         clientes.add(cliente1);
         clientes.add(cliente2);
-        // Agregar más clientes a la lista si es necesario...
-
+        
         cuentas.add(cuenta1);
         cuentas.add(cuenta2);
         cuentas.add(cuenta3);
         cuentas.add(cuenta4);
-        // Agregar más cuentas a la lista si es necesario...
+        
     }
 
     // Agregar métodos para operaciones del cajero (retiro, transferencia, imprimir ticket, etc.)
